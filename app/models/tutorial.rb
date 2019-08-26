608bdb5dd141dd1f2c3f8c45e2e373c78cf740e5
Tay <tay.james.d@gmail.com>
2019-08-25 23:42:34 -0600

    Adds model testing for Tutorial model including dependent destroy

--- app/models/tutorial.rb
+++ app/models/tutorial.rb
@@ -1,5 +1,11 @@
 class Tutorial < ApplicationRecord
-  has_many :videos, ->  { order(position: :ASC) }
+  has_many :videos, ->  { order(position: :ASC) }, dependent: :destroy
   acts_as_taggable_on :tags, :tag_list
   accepts_nested_attributes_for :videos
+
+  validates_presence_of :title
+  validates_presence_of :description
+  # validates_presence_of :thumbnail
+  validates_presence_of :playlist_id
+  validates_presence_of :classroom
 end
