3c0bafca666cb291b1ce696bd5ed5f6e39b79521
Tay <tay.james.d@gmail.com>
2019-08-26 19:41:00 -0600

    Adds testing to ensure that a Tutorial show page won't error out when a tutorial doesn't have any videos

---
+++ spec/features/tutorial_videos_spec.rb
@@ -0,0 +1,18 @@
+require "rails_helper"
+
+RSpec.describe "Tutorial Show Page" do
+  it "A tutorial with no videos won't error out and instead display a message" do
+    tutorial_1 = create(:tutorial)
+
+    visit root_path
+    click_on tutorial_1.title
+    expect(page).to have_content("This tutorial doesn't have any videos!")
+    save_and_open_page
+    video_1 = create(:video, tutorial: tutorial_1)
+
+    visit root_path
+    click_on tutorial_1.title
+    expect(page).to have_content(video_1.title)
+    save_and_open_page
+  end
+end
