525d26df6cf8ed6ba69cd7244062fc726ff8de07
Tay <tay.james.d@gmail.com>
2019-08-26 19:14:04 -0600

    Adds dependent destroy to user_videos relationship and removes thumbmnail validations

--- app/models/video.rb
+++ app/models/video.rb
@@ -1,12 +1,11 @@
 class Video < ApplicationRecord
-  has_many :user_videos
+  has_many :user_videos, dependent: :destroy
   has_many :users, through: :user_videos
   belongs_to :tutorial
 
   validates_presence_of :title
   validates_presence_of :description
   validates_presence_of :video_id
-  validates_presence_of :thumbnail
   validates_presence_of :tutorial_id
   validates_presence_of :position
 end
