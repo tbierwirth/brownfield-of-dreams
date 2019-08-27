7f2518830f7e2400e8dac2376fe73ac34a276307
Tay <tay.james.d@gmail.com>
2019-08-26 18:40:44 -0600

    Adds conditional to TutorialsController Show action to ensure that a user has to be logged in to view certain tutorials

--- app/controllers/tutorials_controller.rb
+++ app/controllers/tutorials_controller.rb
@@ -1,6 +1,7 @@
 class TutorialsController < ApplicationController
   def show
     tutorial = Tutorial.find(params[:id])
+    require_user if tutorial.classroom?
     @facade = TutorialFacade.new(tutorial, params[:video_id])
   end
 end
