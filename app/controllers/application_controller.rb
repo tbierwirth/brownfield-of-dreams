29ed7ffa48019202181ed2a7c12a32670b6b25a5
Tay <tay.james.d@gmail.com>
2019-08-26 18:40:08 -0600

    Adds helper_method require_user

--- app/controllers/application_controller.rb
+++ app/controllers/application_controller.rb
@@ -3,11 +3,17 @@ class ApplicationController < ActionController::Base
   helper_method :find_bookmark
   helper_method :list_tags
   helper_method :tutorial_name
+  helper_method :require_user
+
 
   add_flash_types :success
 
   def current_user
     @current_user ||= User.find(session[:user_id]) if session[:user_id]
+  end
+
+  def require_user
+    render file: 'public/404', status: 404 unless current_user
   end
 
   def find_bookmark(id)
