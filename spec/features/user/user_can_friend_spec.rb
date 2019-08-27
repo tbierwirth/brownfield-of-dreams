b0b4f7af65c49a72217235c692ae2090304360d1
Tyler Bierwirth <tylerbierwirth@gmail.com>
2019-08-27 10:44:00 -0600

    Create test for user adding a friend and add webmocked fixtures
    Co-Authored-By: Tay <tay.james.d@gmail.com>

---
+++ spec/features/user/user_can_friend_spec.rb
@@ -0,0 +1,48 @@
+require 'rails_helper'
+
+RSpec.describe 'User Show Page' do
+  before :each do
+    followers = File.open('./spec/fixtures/followers.json')
+    stub_request(:get, "https://api.github.com/user/followers").
+      to_return(status: 200, body: followers)
+
+    following = File.open('./spec/fixtures/following.json')
+    stub_request(:get, "https://api.github.com/user/following").
+      to_return(status: 200, body: following)
+
+    repos = File.open('./spec/fixtures/repos.json')
+    stub_request(:get, "https://api.github.com/user/repos").
+      to_return(status: 200, body: repos)
+  end
+  it "a user sees an add friend button next to a registered follower" do
+    tyler = create(:user)
+    tay = create(:user)
+    tyler.user_tokens.create(provider: 'github', token: 'ji342n4o2', uid: '46985326')
+    tay.user_tokens.create(provider: 'github', token: 'aknkl424', uid: '47490116')
+    allow_any_instance_of(ApplicationController).to receive(:current_user).and_return(tyler)
+
+    visit dashboard_path
+
+    expect(page).to have_content('Friends')
+
+    within '.friends' do
+      expect(page).to have_content('No friends added')
+    end
+
+    within(first('.follower')) do
+      expect(page).to_not have_button('Add as Friend')
+    end
+
+    expect(page).to have_button('Add as Friend')
+    click_on 'Add as Friend'
+
+    expect(current_path).to eq(dashboard_path)
+
+    expect(page).to_not have_button('Add as Friend')
+
+    within '.friends' do
+      expect(page).to have_css('.friend', count: 1)
+    end
+
+  end
+end
