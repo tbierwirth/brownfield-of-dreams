6f574ce460d17027015c3746d9de11c7620e6560
Tay <tay.james.d@gmail.com>
2019-08-27 20:21:40 -0600

    Adds validation for thumbnail and removes playlist_id validation
    Co-Authored-By: null <tbierwirth@users.noreply.github.com>

--- app/models/tutorial.rb
+++ app/models/tutorial.rb
@@ -5,5 +5,5 @@ class Tutorial < ApplicationRecord
 
   validates_presence_of :title
   validates_presence_of :description
-  validates_presence_of :playlist_id
+  validates_presence_of :thumbnail
 end
