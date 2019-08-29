fe4c435a3a4f6f8270a2679ee30b17ec5d040694
Tay <tay.james.d@gmail.com>
2019-08-29 09:47:41 -0600

    Updates some realtionships and validations for model testing

--- spec/models/tutorial_spec.rb
+++ spec/models/tutorial_spec.rb
@@ -15,6 +15,6 @@ RSpec.describe Tutorial, type: :model do
   describe  "validations" do
     it { should validate_presence_of(:title)}
     it { should validate_presence_of(:description)}
-    it { should validate_presence_of(:playlist_id)}
+    it { should validate_presence_of(:thumbnail)}
   end
 end
